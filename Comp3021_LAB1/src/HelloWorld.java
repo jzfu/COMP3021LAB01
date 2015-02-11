75b44868e84d1df4ba69f22981380cca0af8a3c7
jzfu <jzfu@stu.hk>
2015-02-11 12:30:06 +0800

    Update HelloWorld.java
    test3 on github

--- Comp3021_LAB1/src/HelloWorld.java
+++ Comp3021_LAB1/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world, test 2");
+		System.out.println("Hello world, test 2, test 3 on github");
 		// TODO Auto-generated method stub
 
 	}
